a312d3090acdedbd60027aec6417acdb61445ee3
Oracle_BH <38717741+oraclBH@users.noreply.github.com>
2022-02-20 09:37:04 +0800

    Add files via upload

---
+++ recursive.m
@@ -0,0 +1,35 @@
+
+clc; clear; close all;
+
+% dta = [1,2,3,4,5,6,7,8,9,10];%
+dta = 1:200;
+sum_Data = recursiveReduce(dta, 200);
+disp(sum_Data);
+
+
+function  smout = recursiveReduce(data, size)
+
+if size ==1
+    smout = data(1);
+    return;
+else
+    if mod(size, 2)==0   %% Å¼ÊýÊý×é
+        stride = floor(size/2);
+        for i = 1:stride
+            data(i) = data(i) + data(i+stride);
+        end
+        smout = recursiveReduce(data, stride);
+    else
+         stride = ceil(size/2);
+         data(stride*2) = 0;
+         for i = 1:stride
+            data(i) = data(i) + data(i+stride);
+        end
+        smout = recursiveReduce(data, stride);
+    end
+        
+end
+
+end
+
+
