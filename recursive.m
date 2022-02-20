7e39e90f3d40efd74a2c550498ffc50669b9e883
Oracle_BH <38717741+oraclBH@users.noreply.github.com>
2022-02-20 09:39:37 +0800

    Update recursive.m

--- recursive.m
+++ recursive.m
@@ -1,7 +1,7 @@
 
 clc; clear; close all;
 
-% dta = [1,2,3,4,5,6,7,8,9,10];%
+% dta = [1,2,3,4,5,6,7,8,9,10];
 dta = 1:200;
 sum_Data = recursiveReduce(dta, 200);
 disp(sum_Data);
@@ -13,7 +13,7 @@ if size ==1
     smout = data(1);
     return;
 else
-    if mod(size, 2)==0   %% Å¼ÊýÊý×é
+    if mod(size, 2)==0   % An even number of arrays
         stride = floor(size/2);
         for i = 1:stride
             data(i) = data(i) + data(i+stride);
